6ca599f5e31274a13c4a7a5d681727e94eff679c
Sissel <victorio.sarnaglia@gmail.com>
2023-02-08 11:45:16 -0300

    agrego utils

--- Backend/ecommerce/src/main/java/com/example/ecommerce/model/Role.java
+++ Backend/ecommerce/src/main/java/com/example/ecommerce/model/Role.java
@@ -5,20 +5,20 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.hibernate.annotations.Table;
-import org.springframework.data.annotation.Id;
 
 @Getter
 @Setter
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
-@Table(name = "roles")
+@Table(name="roles")
 public class Role {
 
     @Id
-    @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
     private Long id;
+
+
 
 }
