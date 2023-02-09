a4fa2e4b9c94af05fbde3f7893ad9bc1d128b3c4
pablex72 <tonyp2hoy@gmail.com>
2023-02-08 21:18:24 -0400

    feature category model

---
+++ Backend/ecommerce/src/main/java/com/example/ecommerce/model/Category.java
@@ -0,0 +1,31 @@
+package com.example.ecommerce.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Table(name = "Categories")
+public class Category {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "Name", nullable = false)
+    private String Name;
+
+    @Column(name = "Description", nullable = false)
+    private String Description;
+    
+}
